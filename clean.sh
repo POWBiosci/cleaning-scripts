d87d1c9c0d8f72daad1cead4469339be634938c4
agcardia <69215642+agcardia@users.noreply.github.com>
2022-07-29 16:46:57 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,18 +3,6 @@
 #user input filename
 read -p 'Input Filename: ' FILE
 
-#awk basics
-#print row (FNR==n for nth row)
-#awk -F ',' 'FNR == 2 { print }' $FILE 
-#print column, (print $NF for last column)
-#awk -F "," '{ print $NF }' $FILE 
-
-#get last column number
-#last=$(awk -F "," '{ print NF }' $FILE | sort -nu | head -n 1)
-
-#remove first and last column
-#gcut --complement -d "," -f$last,1 $FILE  > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
-
 #remove 1st row
 tail -n +2 $FILE  > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 
